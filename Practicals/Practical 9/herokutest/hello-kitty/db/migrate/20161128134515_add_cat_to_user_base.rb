de6016e30740587949b9875eb50fd9f915774c97
Pauline Lallinec <PaulineLc@users.noreply.github.com>
2016-11-28 15:39:03 +0000

    moved a folder

--- Practicals/Practical 9/herokutest/hello-kitty/db/migrate/20161128134515_add_cat_to_user_base.rb
+++
@@ -1,5 +0,0 @@
-class AddCatToUserBase < ActiveRecord::Migration[5.0]
-  def change
-    add_column :user_bases, :cat, :string
-  end
-end
--- Practicals/Practical 9/herokutest/hello-kitty/test/controllers/adoption_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AdoptionControllerTest < ActionDispatch::IntegrationTest
-  test "should get kittenlisting" do
-    get adoption_kittenlisting_url
-    assert_response :success
-  end
-
-end
